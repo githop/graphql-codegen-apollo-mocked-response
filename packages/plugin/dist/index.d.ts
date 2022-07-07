c5c3054dbd17fd74b37dacc15a5ba21061959a5c
githop <tom@githop.com>
2022-07-06 18:08:23 -0600

    whoops - rm build aritifacts

--- packages/plugin/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PluginFunction } from '@graphql-codegen/plugin-helpers';
-export interface Config {
-    typesFile: string;
-    addTypename: boolean;
-    prefix?: string;
-}
-export declare function getConfig(config?: Partial<Config>): Config;
-export declare const plugin: PluginFunction<Partial<Config>>;
